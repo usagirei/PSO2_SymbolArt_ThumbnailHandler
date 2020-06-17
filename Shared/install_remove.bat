76676c2e3cb4e417820758fded24abba6e92007e
Reisen Usagi <usagirei@users.noreply.github.com>
2020-06-17 01:44:43 -0300

    Update install script

--- Shared/install_remove.bat
+++ Shared/install_remove.bat
@@ -1,4 +1,41 @@
 @echo off
+
+REM ------------------------------ UAC Boilerplate -----------------------------
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Requesting privilege escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+REM -----------------------------------------------------------
+
 reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=x86 || set OS=x64
 reg query "HKCR\CLSID" | find /i "{63424DF1-FAA8-4598-97E5-6E95D4A4ED67}" > NUL && goto REMOVE || goto INSTALL
 
@@ -7,7 +44,7 @@ echo Installing Thumbnail Handler (%OS%)
 regsvr32 Pso2SarThumbnailHandler.%OS%.dll
 goto END
 :REMOVE
-echo Installing Thumbnail Handler (%OS%)
+echo Removing Thumbnail Handler (%OS%)
 regsvr32 /u Pso2SarThumbnailHandler.%OS%.dll
 goto END
 :END
