f5ca4c2881da8d90c2db3822f2be3a424ae0f448
Reisen Usagi <usagirei@users.noreply.github.com>
2020-06-16 22:06:39 -0300

    Update Test App

--- TestShared/Test.cpp
+++ TestShared/Test.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <Shlwapi.h>
 #include <iostream>
+#include <memory>
 
 #include "stb_image.h"
 #include "stb_image_write.h"
@@ -13,32 +14,93 @@
 
 HINSTANCE g_hInstDll;
 
+
+int fileExists(LPCWSTR file)
+{
+	WIN32_FIND_DATA FindFileData;
+	HANDLE handle = FindFirstFileW(file, &FindFileData);
+	int found = handle != INVALID_HANDLE_VALUE;
+	if (found)
+	{
+		//FindClose(&handle); this will crash
+		FindClose(handle);
+	}
+	return found;
+}
+
+struct ComDeleter {
+	void operator()(IUnknown* unk) {
+		unk->Release();
+	}
+};
+
+template<typename T>
+using com_ptr = std::unique_ptr<T, ComDeleter>;
+
 int wmain(int argc, LPCWSTR* argv)
 {
-	IStream* stream;
-	HRESULT hr = SHCreateStreamOnFile(argv[1], STGM_READ, &stream);
-
-	if (!SUCCEEDED(hr)) {
-		std::cout << "Error reading input";
-		exit(1);
-	}
-
 	WCHAR dstName[MAX_PATH];
-	wcscpy_s(dstName, argv[1]);
-
-	SarFile sar(stream);
-	stream->Release();
-
 	OpenGLRenderer renderer;
+	std::vector<uint32_t> buf;
+	int targetSize = 0;
 
-	int zoom = 4;
-	int sw = sar.width() * zoom;
-	int sh = sar.height() * zoom;
+	for (int i = 1; i < argc; i++) {
 
-	uint8_t* data = new uint8_t[sw * sh * 4];
-	renderer.Render(sar, sw, sh, data);
+		LPCWSTR f = argv[i];
+		int newSize;
+		if (fileExists(f)) {
 
-	stbi_write_png("dump.png", sw, sh, 4, data, sw * 4);
+			wcscpy_s(dstName, f);
+			LPWSTR dot = wcsrchr(dstName, L'.');
+			*dot = '\0';
+			wcscat_s(dstName, L".png");
 
-	
+			IStream* tmp;
+			HRESULT hr = SHCreateStreamOnFileW(argv[1], STGM_READ, &tmp);
+			if (!SUCCEEDED(hr)) {
+				std::cout << "Failed to Open SAR File";
+				continue;
+			}
+
+			com_ptr<IStream> stream(tmp);
+
+			std::unique_ptr<SarFile> sar;
+			try {
+				sar = std::make_unique<SarFile>(stream.get());
+			}
+			catch (sar_exception& e) {
+				std::cout << e.what() << std::endl;
+				continue;
+			}
+
+			int sw = sar->width();
+			int sh = sar->height();
+			if (targetSize) {
+				int maxDim = __max(sw, sh);
+				sw = sw * targetSize / maxDim;
+				sh = sh * targetSize / maxDim;
+			}
+
+			int numPixels = sw * sh;
+			buf.resize(numPixels);
+			renderer.Render(*sar, sw, sh, &buf[0]);
+
+			for (auto p = buf.begin(); p < buf.end(); p++) {
+				uint32_t pixel = *p;
+				pixel = _byteswap_ulong(pixel);
+				pixel = (pixel >> 8) | (pixel << 24);
+				*p = pixel;
+			}
+
+			FILE* outFile;
+			_wfopen_s(&outFile, dstName, L"wb");
+			stbi_write_func* writeFunc = [](void* ctx, void* data, int size) { fwrite(data, 1, size, static_cast<FILE*>(ctx)); };
+			stbi_write_png_to_func(writeFunc, outFile, sw, sh, 4, &buf[0], sw * 4);
+			fclose(outFile);
+
+		}
+		else if ((newSize = wcstol(f, nullptr, 10)) != 0) {
+			targetSize = newSize < 0 ? 0 : newSize;
+		}
+	}
 }
